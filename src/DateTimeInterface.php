5aa1cd6312771bf81df05e0263d4604bf0e52a16
Thomas Koch <koch@comsolit.com>
2014-11-17 15:51:45 +0100

    add return type annotations proposed by scrutinizer

--- src/DateTimeInterface.php
+++ src/DateTimeInterface.php
@@ -3,9 +3,25 @@
 interface DateTimeInterface
 {
     public function diff(DateTimeInterface $datetime2, $absolute = false);
+
+    /**
+     * @return string
+     */
     public function format($format);
+
+    /**
+     * @return integer
+     */
     public function getOffset();
     public function getTimestamp();
+
+    /**
+     * @return DateTimeZone
+     */
     public function getTimezone();
+
+    /**
+     * @return void
+     */
     public function __wakeup();
 }
